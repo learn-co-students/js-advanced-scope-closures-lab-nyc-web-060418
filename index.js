d24662a3488553011bb290b4a264740777d39a0f
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-07-16 22:56:46 -0400

    done

---
+++ index.js
@@ -0,0 +1,33 @@
+ function produceDrivingRange(blockRange) {
+  return function(pointA, pointB) {
+    let distance = parseInt(pointB) - parseInt(pointA)
+    diff = distance - blockRange
+    // debugger
+    if (diff > 0) {
+      return `${diff} blocks out of range`
+    } else {
+      return `within range by ${Math.abs(diff)}`
+    }
+  }
+}
+//test to see if i can call closure like this
+// function combination(pointA,pointB, blocks) {
+//     var test = produceDrivingRange(blocks)
+//     return test(pointA, pointB)
+// }
+
+function produceTipCalculator(percentage) {
+  return function (cost) {
+    return cost * percentage
+  }
+}
+function createDriver() {
+  let driverId = 0
+  // return the class
+  return class {
+    constructor(name){
+      this.name = name
+      this.id = ++driverId;
+    }
+  }
+}
